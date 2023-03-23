2e6eab2c528f418a82d423b93d3194664282f45d
Alexander Bogdanov <a@bogdanov.co>
2023-03-24 04:27:39 +0500

    ✨ Add grpc server

--- cmd/helicopter/main.go
+++ cmd/helicopter/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"helicopter/internal/grpc"
 	"helicopter/internal/mockstorage"
 	"helicopter/internal/rest"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -15,10 +20,23 @@ func main() {
 		os.Exit(1)
 	}
 
+	g, _ := errgroup.WithContext(context.Background())
+
 	rest := rest.NewRest(storage)
-	err = rest.Run("localhost:8080")
-	if err != nil {
-		log.Println("Error while running http server:", err)
-		os.Exit(1)
+	g.Go(func() error {
+		if err := rest.Run("0.0.0.0:8080"); err != nil {
+			return fmt.Errorf("error while running rest server: %v", err)
+		}
+		return nil
+	})
+	rpc := grpc.NewGrpc(storage)
+	g.Go(func() error {
+		if err := rpc.Run("0.0.0.0:1337"); err != nil {
+			return fmt.Errorf("error while running rpc server: %v", err)
+		}
+		return nil
+	})
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
 	}
 }
